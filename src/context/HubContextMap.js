0fccefc380f96021f416155efc149e2f7161e72a
Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>
2025-12-26 14:41:53 -0700

    Create HubContextMap.js

---
+++ src/context/HubContextMap.js
@@ -0,0 +1,25 @@
+// Path: src/context/HubContextMap.js
+
+export const HubContextMap = Object.freeze({
+  Academy: "academy",
+  AcademyEmbedded: "academy-embedded",
+  AssetProvider: "asset-provider",
+  ClassroomTraining: "classroom-training",
+  CmsDevelopers: "cms-developers",
+  CmsFree: "cms-free",
+  Commerce: "commerce",
+  Crm: "crm",
+  Developers: "developers",
+  DirectoryListing: "directory-listing",
+  FbCrm: "fb-crm",
+  HubSpotPartners: "hubspot-partners",
+  HubspotForStartups: "hubspot-for-startups",
+  Integrations: "integrations",
+  Marketing: "marketing",
+  Microapp: "microapp",
+  Purchase: "purchase",
+  Sales: "sales",
+  Service: "service",
+  SolutionsProvider: "solutions-provider",
+  Trial: "trial"
+});
